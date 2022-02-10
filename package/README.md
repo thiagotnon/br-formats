da65d28ccf62919b860e34e5172792610ea92ce7
Thiago Natan <thiagotnon@gmail.com>
2022-02-10 00:14:48 -0300

    add CNPJ format

--- package/README.md
+++ package/README.md
@@ -1,5 +1,13 @@
 # br-formats 🇧🇷 
 
+#### Supported format
+
+Phone: 00000000000 -> (00) 99999-9999
+
+CPF: 00000000000 -> 000.000.000-00
+
+CNPJ: 00.000.000/0000-00
+
 #### How to use
 ```
 //Import the format type
--- package/index.js
+++ package/index.js
@@ -12,4 +12,19 @@ function formatPhoneNumber (phone) {
    return null
 }
 
-module.exports = formatPhoneNumber;
+function formatCnpjNumber (cnpj) {
+   //Filter only numbers from the input
+   let cleaned = ('' + cnpj).replace(/\D/g, '');
+  
+   //Check if the input is of correct length
+   let match = cleaned.match(/^(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})$/);
+ 
+   if (match) {
+     return match[1] + '.' + match[2] + '.' + match[3] + '/' + match[4] + '-' + match[5]
+   };
+ 
+   return null
+}
+
+
+module.exports = { formatPhoneNumber, formatCnpjNumber };
